fb7dadec87d30580c9b0450e7e2889e9fd55d195
JAGADEESWARARAO B <84315990+smartjaganrao@users.noreply.github.com>
2026-03-07 21:26:49 +0530

    feat: Add party logos to assets
    Adds PNG images for various political party logos to the assets directory. This will allow for visual representation of parties within the application.

--- src/data/parties.ts
+++ src/data/parties.ts
@@ -1,4 +1,4 @@
-const imageModules = import.meta.glob('../assets/img/*.{webp,avif}', { eager: true, query: '?url', import: 'default' });
+const imageModules = import.meta.glob('../assets/img/*.{webp,avif,jpg}', { eager: true, query: '?url', import: 'default' });
 
 const getPartyImage = (filename: string) => {
   const path = `../assets/img/${filename}`;
@@ -19,39 +19,39 @@ export interface Party {
 
 export const parties: Party[] = [
   // RULING FRONT (SPA)
-  { id: "DMK", name: "DMK", color: "#FF0000", image: getPartyImage('DMK.webp'), axis: 'RULING' },
-  { id: "Congress", name: "INC", color: "#0000FF", image: getPartyImage('INC.avif'), axis: 'RULING' },
-  { id: "VCK", name: "VCK", color: "#0000FF", image: getPartyImage('VCK.webp'), axis: 'RULING' },
-  { id: "CPI", name: "CPI", color: "#FF0000", image: getPartyImage('CPI.avif'), axis: 'RULING' },
-  { id: "CPIM", name: "CPI(M)", color: "#FF0000", image: getPartyImage('CPIM.avif'), axis: 'RULING' },
-  { id: "MDMK", name: "MDMK", color: "#FF0000", image: getPartyImage('MDMK.webp'), axis: 'RULING' },
-  { id: "IUML", name: "IUML", color: "#008000", image: getPartyImage('IUML.webp'), axis: 'RULING' },
-  { id: "KMDK", name: "KMDK", color: "#FFFF00", image: getPartyImage('KMDK.webp'), axis: 'RULING' },
-  { id: "MMK", name: "MMK", color: "#008000", image: getPartyImage('MMK.webp'), axis: 'RULING' },
+  { id: "DMK", name: "DMK", color: "#FF0000", image: getPartyImage('DMK.jpg'), axis: 'RULING' },
+  { id: "Congress", name: "INC", color: "#0000FF", image: getPartyImage('INC.jpg'), axis: 'RULING' },
+  { id: "VCK", name: "VCK", color: "#0000FF", image: getPartyImage('VCK.jpg'), axis: 'RULING' },
+  { id: "CPI", name: "CPI", color: "#FF0000", image: getPartyImage('CPI.jpg'), axis: 'RULING' },
+  { id: "CPIM", name: "CPI(M)", color: "#FF0000", image: getPartyImage('CPIM.jpg'), axis: 'RULING' },
+  { id: "MDMK", name: "MDMK", color: "#FF0000", image: getPartyImage('MDMK.jpg'), axis: 'RULING' },
+  { id: "IUML", name: "IUML", color: "#008000", image: getPartyImage('IUML.jpg'), axis: 'RULING' },
+  { id: "KMDK", name: "KMDK", color: "#FFFF00", image: getPartyImage('KMDK.jpg'), axis: 'RULING' },
+  { id: "MMK", name: "MMK", color: "#008000", image: getPartyImage('MMK.jpg'), axis: 'RULING' },
   
   // OPPOSITION FRONT (AIADMK/NDA)
-  { id: "AIADMK", name: "AIADMK", color: "#008000", image: getPartyImage('AIDMK.webp'), axis: 'OPPOSITION' },
-  { id: "BJP", name: "BJP", color: "#FF9933", image: getPartyImage('BJP.webp'), axis: 'OPPOSITION' },
-  { id: "PMK", name: "PMK", color: "#FFFF00", image: getPartyImage('PMK.webp'), axis: 'OPPOSITION' },
-  { id: "DMDK", name: "DMDK", color: "#FFD700", image: getPartyImage('DMDK.avif'), axis: 'OPPOSITION' },
-  { id: "AMMK", name: "AMMK", color: "#FF0000", image: getPartyImage('AMMK.webp'), axis: 'OPPOSITION' },
-  { id: "TMCM", name: "TMC(M)", color: "#FF9933", image: getPartyImage('TMC.webp'), axis: 'OPPOSITION' },
-  { id: "PNK", name: "PNK", color: "#FF0000", image: getPartyImage('PNK.webp'), axis: 'OPPOSITION' },
-  { id: "PBK", name: "PBK", color: "#FF0000", image: getPartyImage('PBK.webp'), axis: 'OPPOSITION' },
-  { id: "IJK", name: "IJK", color: "#FF0000", image: getPartyImage('IJK.webp'), axis: 'OPPOSITION' },
+  { id: "AIADMK", name: "AIADMK", color: "#008000", image: getPartyImage('AIADMK.jpg'), axis: 'OPPOSITION' },
+  { id: "BJP", name: "BJP", color: "#FF9933", image: getPartyImage('BJP.jpg'), axis: 'OPPOSITION' },
+  { id: "PMK", name: "PMK", color: "#FFFF00", image: getPartyImage('PMK.jpg'), axis: 'OPPOSITION' },
+  { id: "DMDK", name: "DMDK", color: "#FFD700", image: getPartyImage('DMDK.jpg'), axis: 'OPPOSITION' },
+  { id: "AMMK", name: "AMMK", color: "#FF0000", image: getPartyImage('AMMK.jpg'), axis: 'OPPOSITION' },
+  { id: "TMCM", name: "TMC(M)", color: "#FF9933", image: getPartyImage('TMC.jpg'), axis: 'OPPOSITION' },
+  { id: "PNK", name: "PNK", color: "#FF0000", image: getPartyImage('PNK.jpg'), axis: 'OPPOSITION' },
+  { id: "PBK", name: "PBK", color: "#FF0000", image: getPartyImage('PBK.jpg'), axis: 'OPPOSITION' },
+  { id: "IJK", name: "IJK", color: "#FF0000", image: getPartyImage('IJK.jpg'), axis: 'OPPOSITION' },
 
   // ALTERNATIVES & OTHERS
-  { id: "TVK", name: "TVK", color: "#800000", image: getPartyImage('TVK.webp'), axis: 'ALTERNATIVES' },
-  { id: "NTK", name: "NTK", color: "#FFFF00", image: getPartyImage('NTK.webp'), axis: 'ALTERNATIVES' },
-  { id: "MNM", name: "MNM", color: "#FFFFFF", image: getPartyImage('MNM.webp'), axis: 'ALTERNATIVES' },
-  { id: "SDPI", name: "SDPI", color: "#008000", image: getPartyImage('SDPI.webp'), axis: 'ALTERNATIVES' },
-  { id: "AAP", name: "AAP", color: "#0000FF", image: getPartyImage('AAP.webp'), axis: 'ALTERNATIVES' },
-  { id: "PT", name: "PT", color: "#FF0000", image: getPartyImage('PT.webp'), axis: 'ALTERNATIVES' },
-  { id: "TMMK", name: "TMMK", color: "#008000", image: getPartyImage('TMMK.webp'), axis: 'ALTERNATIVES' },
-  { id: "ATP", name: "ATP", color: "#FF0000", image: getPartyImage('ATP.webp'), axis: 'ALTERNATIVES' },
-  { id: "INL", name: "INL", color: "#008000", image: getPartyImage('INL.webp'), axis: 'ALTERNATIVES' },
-  { id: "BSP", name: "BSP", color: "#0000FF", image: getPartyImage('BSP.webp'), axis: 'ALTERNATIVES' },
-  { id: "AIFB", name: "AIFB", color: "#FF0000", image: getPartyImage('AIFB.webp'), axis: 'ALTERNATIVES' },
-  { id: "NOTA", name: "NOTA", color: "#808080", image: getPartyImage('NOTA.webp'), axis: 'ALTERNATIVES' }
+  { id: "TVK", name: "TVK", color: "#800000", image: getPartyImage('TVK.jpg'), axis: 'ALTERNATIVES' },
+  { id: "NTK", name: "NTK", color: "#FFFF00", image: getPartyImage('NTK.jpg'), axis: 'ALTERNATIVES' },
+  { id: "MNM", name: "MNM", color: "#FFFFFF", image: getPartyImage('MNM.jpg'), axis: 'ALTERNATIVES' },
+  { id: "SDPI", name: "SDPI", color: "#008000", image: getPartyImage('SDPI.jpg'), axis: 'ALTERNATIVES' },
+  { id: "AAP", name: "AAP", color: "#0000FF", image: getPartyImage('AAP.jpg'), axis: 'ALTERNATIVES' },
+  { id: "PT", name: "PT", color: "#FF0000", image: getPartyImage('PT.jpg'), axis: 'ALTERNATIVES' },
+  { id: "TMMK", name: "TMMK", color: "#008000", image: getPartyImage('TMMK.jpg'), axis: 'ALTERNATIVES' },
+  { id: "ATP", name: "ATP", color: "#FF0000", image: getPartyImage('ATP.jpg'), axis: 'ALTERNATIVES' },
+  { id: "INL", name: "INL", color: "#008000", image: getPartyImage('INL.jpg'), axis: 'ALTERNATIVES' },
+  { id: "BSP", name: "BSP", color: "#0000FF", image: getPartyImage('BSP.jpg'), axis: 'ALTERNATIVES' },
+  { id: "AIFB", name: "AIFB", color: "#FF0000", image: getPartyImage('AIFB.jpg'), axis: 'ALTERNATIVES' },
+  { id: "NOTA", name: "NOTA", color: "#808080", image: getPartyImage('NOTA.jpg'), axis: 'ALTERNATIVES' }
 ];
 
